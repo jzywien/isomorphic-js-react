124d57604ad9e70874a2bd1221bbead972d867ac
jzywien <jzywien@gmail.com>
2017-05-10 08:57:13 -0400

    fix history warnings

--- shared/routes.jsx
+++ shared/routes.jsx
@@ -1,6 +1,7 @@
 import React     from 'react';
 import { Route } from 'react-router';
 import AppView from './components/AppView.jsx';
+
 export default (
   <Route name="app" component={AppView} path="/">
   </Route>
